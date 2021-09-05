c547e99799a3ea14174b3ce43d82b4bc952f04e1
Oren Scheer <oren.rs723@gmail.com>
2021-09-04 22:39:36 -0400

    Change layout

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,12 @@ import {
   useColorModeValue,
   Text,
 } from "@chakra-ui/react";
-import { FaGithub, FaLinkedin } from "react-icons/all";
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/all";
 import ColorModeSwitcher from "./ColorModeSwitcher";
 import Pine from "./images/Pine.png";
 
 const App: React.FC = () => {
-  const teal = useColorModeValue("#38B2AC", "#81E6D9");
+  const titleColour = useColorModeValue("#458EB2", "#87CDE5");
   return (
     <Box
       textAlign="center"
@@ -48,25 +48,40 @@ const App: React.FC = () => {
           <ColorModeSwitcher zIndex="9" />
         </Flex>
       </Flex>
-      <Flex direction="column" alignItems="center">
+      <Flex direction="column" alignItems="center" justifyContent="center">
         <Image
           borderRadius="full"
           boxSize="150px"
           src={Pine}
           alt="Oren Scheer"
         />
-        <Heading color={teal} size="3xl" mt={3} mb={4}>
+        <Heading color={titleColour} size="3xl" mt={3} mb={4}>
           Oren Scheer
         </Heading>
-        <Flex justifyContent="space-between" width="250px">
+        <Flex justifyContent="space-between" width="450px">
           <Link href="https://linkedin.com/in/orenscheer" isExternal>
-            <Button leftIcon={<FaLinkedin />} colorScheme="teal">
+            <Button
+              leftIcon={<FaLinkedin />}
+              variant="link"
+              color={titleColour}
+            >
               LinkedIn
             </Button>
           </Link>
+          <Text>/</Text>
           <Link href="https://github.com/orenscheer" isExternal>
-            <Button leftIcon={<FaGithub />} colorScheme="teal">
+            <Button leftIcon={<FaGithub />} variant="link" color={titleColour}>
               GitHub
+            </Button>
+          </Link>
+          <Text>/</Text>
+          <Link href="mailto:contact@orenscheer.me" isExternal>
+            <Button
+              leftIcon={<FaEnvelope />}
+              variant="link"
+              color={titleColour}
+            >
+              contact@orenscheer.me
             </Button>
           </Link>
         </Flex>
@@ -74,24 +89,24 @@ const App: React.FC = () => {
           I&apos;m in my third year at the University of Ottawa, studying
           computer science and math.
         </Text>
-        <Flex alignItems="center">
+        <Flex direction="column" alignItems="center">
           <Link href="https://nextavailableread.orenscheer.me" isExternal>
-            <Button variant="ghost" colorScheme="teal">
+            <Button variant="outline" colorScheme="teal">
               NextAvailableRead
             </Button>
           </Link>
-          <Text>/</Text>
           <Link
             href="https://github.com/orenscheer/find-my-representatives"
             isExternal
+            mt={3}
+            mb={3}
           >
-            <Button variant="ghost" color="#3899A0">
+            <Button variant="outline" colorScheme="blue">
               Find My Representatives
             </Button>
           </Link>
-          <Text>/</Text>
           <Link href="https://travelorange.orenscheer.me" isExternal>
-            <Button variant="ghost" colorScheme="orange">
+            <Button variant="outline" colorScheme="orange">
               Travel Orange
             </Button>
           </Link>
